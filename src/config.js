8dd5b9caa589457a1e39920ef9da605e0d9dd174
ZEROSONES\Benedikt Böhm <Benedikt.Boehm@diva-e.com>
2017-11-30 11:13:11 +0100

    add drage n drop; add resize; add customizable text

--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
 const config = {
   author: "Benedikt Böhm",
   title: "diva-e Christmas Card",
-  instructions: "Set params and press refresh. <br><br> Min and max angles: The amount of angles each star possesses. <br> Min and max length: The size of the stars. <br> Min and max repetitions: The length of the arms of each star."
+  instructions: "Set params and press refresh. " +
+  "<br>You can drag and drop elements." +
+  "<br>You can resize elements by clicking them, then scrolling while mouse down" +
+  "<br>" +
+  "<br>" +
+  "<br>Parameters:" +
+  "<br> Color: If starts with '#', show diva-e logo, otherwise hide it (#ffffff vs. ffffff)" +
+  "<br> Min and max angles: The amount of angles each star possesses. " +
+  "<br> Min and max length: The size of the stars. " +
+  "<br> Min and max repetitions: The length of the arms of each star."
 };
 
 export default config;
--- src/js/my-code/index.js
+++ src/js/my-code/index.js
@@ -19,9 +19,6 @@ class Artwork {
             maxRepetitions: 9,
             count: 6,
         };
-        this.logoPath = (this.params.color === '#ffffff')
-            ? '<path transform="matrix(2 0 0 2 690 20)" d="M55.99 3.08c-.43.44-.68 1.06-.74 1.86h4.58c-.02-.42-.09-.79-.22-1.11-.13-.32-.3-.59-.51-.8-.21-.21-.46-.36-.73-.47-.27-.1-.55-.15-.84-.15-.6 0-1.11.22-1.54.67zm2.96 6.3c.37-.23.61-.54.72-.95h3.06c-.35 1.19-.96 2.1-1.83 2.74-.86.64-1.9.96-3.1.96-3.74 0-5.61-2.09-5.61-6.28 0-.89.12-1.69.37-2.41.24-.72.59-1.33 1.06-1.85.46-.51 1.03-.91 1.7-1.18.66-.27 1.43-.41 2.3-.41 1.73 0 3.04.57 3.93 1.71.89 1.14 1.34 2.86 1.34 5.15h-7.73c.04.88.29 1.57.74 2.09.45.51 1.03.77 1.76.77.49 0 .92-.11 1.29-.34zm-12.58 4.64c-.21.21-.52.39-.92.52-.39.13-.97.27-1.73.42-1.18.23-1.98.54-2.43.9-.43.37-.66.9-.66 1.59 0 1.18.66 1.78 1.99 1.78.52 0 1.01-.09 1.46-.28.45-.18.84-.43 1.17-.74.32-.31.59-.68.79-1.1.2-.42.3-.87.3-1.34zM49 7.11c.51.53.88 1.19 1.12 1.97.23.79.34 1.68.34 2.67v10.07h-3.81c-.12-.41-.21-.89-.25-1.43-.48.56-1.13 1.01-1.92 1.35-.8.33-1.72.5-2.77.5-1.79 0-3.1-.41-3.93-1.22-.83-.81-1.24-1.86-1.24-3.15 0-1.14.18-2.06.53-2.74.36-.68.85-1.21 1.49-1.6.63-.39 1.38-.67 2.25-.85.87-.19 1.82-.36 2.85-.51 1.03-.15 1.72-.35 2.08-.59.35-.25.53-.66.53-1.22 0-.52-.23-.92-.71-1.21-.47-.29-1.1-.44-1.9-.44-.95 0-1.62.22-2.03.65-.41.43-.67 1-.77 1.71h-3.82c.02-.82.16-1.57.4-2.26.24-.69.63-1.28 1.16-1.78.54-.5 1.24-.88 2.1-1.15.86-.27 1.91-.4 3.15-.4 1.21 0 2.25.14 3.1.42.85.28 1.53.68 2.05 1.21zM31.66 21.82h-4.29L21.83 5.9h4.57l3.24 10.51L32.92 5.9h4.31zM16.73 5.9h4.09v15.92h-4.09zm0-5.83h4.09v3.92h-4.09zM7.65 18.81c-1.03 0-1.87-.45-2.52-1.35-.65-.89-.97-2.05-.97-3.47 0-3.28 1.17-4.92 3.53-4.92 2.28 0 3.43 1.72 3.43 5.17 0 1.36-.33 2.46-.99 3.3-.66.85-1.49 1.27-2.48 1.27zm3.31-10.84c-1.15-1.66-2.69-2.49-4.6-2.49-1 0-1.9.2-2.69.59-.79.4-1.46.97-2 1.7-.55.74-.96 1.6-1.24 2.61-.29 1-.43 2.11-.43 3.31 0 1.34.17 2.54.5 3.6.34 1.05.8 1.95 1.39 2.68.59.74 1.29 1.3 2.11 1.68.82.39 1.72.59 2.71.59 1.93 0 3.37-.79 4.31-2.37l.04 1.95h3.93V.07h-4.03zM52 15.97h10.63V14H52z" fill="#fff"/>'
-            : '';
     }
 
     init() {
@@ -30,18 +27,11 @@ class Artwork {
     }
 
     redrawSvg(){
-console.log('abstract', this.params.abstract);
-
-        this.Paths = [];
-
-        for (let i=0; i<this.params.count; i++) {
-            const xOffset = Math.random() * this.width;
-            const yOffset = Math.random() * this.height;
-            this.Paths.push('<g class="flower">');
-            let length = Math.random() * (this.params.maxLength - this.params.minLength) + this.params.minLength;
-
-            this.createStar(xOffset, yOffset, length);
-            this.Paths.push('</g>');
+        this.logoPath = (this.params.color[0] === '#')
+            ? '<path transform="matrix(2 0 0 2 690 20)" d="M55.99 3.08c-.43.44-.68 1.06-.74 1.86h4.58c-.02-.42-.09-.79-.22-1.11-.13-.32-.3-.59-.51-.8-.21-.21-.46-.36-.73-.47-.27-.1-.55-.15-.84-.15-.6 0-1.11.22-1.54.67zm2.96 6.3c.37-.23.61-.54.72-.95h3.06c-.35 1.19-.96 2.1-1.83 2.74-.86.64-1.9.96-3.1.96-3.74 0-5.61-2.09-5.61-6.28 0-.89.12-1.69.37-2.41.24-.72.59-1.33 1.06-1.85.46-.51 1.03-.91 1.7-1.18.66-.27 1.43-.41 2.3-.41 1.73 0 3.04.57 3.93 1.71.89 1.14 1.34 2.86 1.34 5.15h-7.73c.04.88.29 1.57.74 2.09.45.51 1.03.77 1.76.77.49 0 .92-.11 1.29-.34zm-12.58 4.64c-.21.21-.52.39-.92.52-.39.13-.97.27-1.73.42-1.18.23-1.98.54-2.43.9-.43.37-.66.9-.66 1.59 0 1.18.66 1.78 1.99 1.78.52 0 1.01-.09 1.46-.28.45-.18.84-.43 1.17-.74.32-.31.59-.68.79-1.1.2-.42.3-.87.3-1.34zM49 7.11c.51.53.88 1.19 1.12 1.97.23.79.34 1.68.34 2.67v10.07h-3.81c-.12-.41-.21-.89-.25-1.43-.48.56-1.13 1.01-1.92 1.35-.8.33-1.72.5-2.77.5-1.79 0-3.1-.41-3.93-1.22-.83-.81-1.24-1.86-1.24-3.15 0-1.14.18-2.06.53-2.74.36-.68.85-1.21 1.49-1.6.63-.39 1.38-.67 2.25-.85.87-.19 1.82-.36 2.85-.51 1.03-.15 1.72-.35 2.08-.59.35-.25.53-.66.53-1.22 0-.52-.23-.92-.71-1.21-.47-.29-1.1-.44-1.9-.44-.95 0-1.62.22-2.03.65-.41.43-.67 1-.77 1.71h-3.82c.02-.82.16-1.57.4-2.26.24-.69.63-1.28 1.16-1.78.54-.5 1.24-.88 2.1-1.15.86-.27 1.91-.4 3.15-.4 1.21 0 2.25.14 3.1.42.85.28 1.53.68 2.05 1.21zM31.66 21.82h-4.29L21.83 5.9h4.57l3.24 10.51L32.92 5.9h4.31zM16.73 5.9h4.09v15.92h-4.09zm0-5.83h4.09v3.92h-4.09zM7.65 18.81c-1.03 0-1.87-.45-2.52-1.35-.65-.89-.97-2.05-.97-3.47 0-3.28 1.17-4.92 3.53-4.92 2.28 0 3.43 1.72 3.43 5.17 0 1.36-.33 2.46-.99 3.3-.66.85-1.49 1.27-2.48 1.27zm3.31-10.84c-1.15-1.66-2.69-2.49-4.6-2.49-1 0-1.9.2-2.69.59-.79.4-1.46.97-2 1.7-.55.74-.96 1.6-1.24 2.61-.29 1-.43 2.11-.43 3.31 0 1.34.17 2.54.5 3.6.34 1.05.8 1.95 1.39 2.68.59.74 1.29 1.3 2.11 1.68.82.39 1.72.59 2.71.59 1.93 0 3.37-.79 4.31-2.37l.04 1.95h3.93V.07h-4.03zM52 15.97h10.63V14H52z" fill="#fff"/>'
+            : '';
+        if(this.params.color[0] !== '#'){
+            this.params.color = '#' + this.params.color;
         }
 
         this.svg.innerHTML = ''
@@ -52,30 +42,91 @@ console.log('abstract', this.params.abstract);
             + '<feComposite in="SourceGraphic" in2="light" operator="arithmetic" k1="1" k2="0" k3="0" k4="0"/>'
             + '</filter>'
             + '<rect fill="' + this.params.backgroundColor + '" x="0" y="0" width="' + this.width + '" height="' + this.height + '" filter="url(#lightMe1)"/>'
-            + this.Paths.join(' ')
             + this.logoPath;
+
+        this.matrix = this.matrix || {};
+
+        for (let i=0; i<this.params.count; i++) {
+            const xOffset = Math.random() * this.width;
+            const yOffset = Math.random() * this.height;
+            this.Paths.push('<g class="flower">');
+            let length = Math.random() * (this.params.maxLength - this.params.minLength) + this.params.minLength;
+
+            let gElement = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+            gElement.setAttribute('class', 'star');
+            gElement.setAttribute('id', 'star_' + i);
+
+            this.matrix[gElement.id] = [1, 0, 0, 1, 0, 0];
+
+            //select current element as active
+            gElement.addEventListener('mousedown', (e) => {
+                this.activeElement = gElement.id;
+                this.matrixX = this.matrix[gElement.id][4];
+                this.matrixY = this.matrix[gElement.id][5];
+                this.x = e.clientX;
+                this.y = e.clientY;
+            });
+
+            //deselect
+            gElement.addEventListener('mouseup', () => {
+                this.activeElement = null;
+            });
+
+            //move selected element
+            gElement.addEventListener('mousemove', (e) => {
+                if(this.activeElement === gElement.id){
+                    this.matrix[gElement.id][4] = this.matrixX + e.clientX - this.x;
+                    this.matrix[gElement.id][5] = this.matrixY + e.clientY - this.y;
+                    gElement.setAttribute('transform', 'matrix(' + this.matrix[gElement.id].join(' ') + ')');
+                }
+            });
+
+            //resize selected element
+            gElement.addEventListener('mousewheel', (e) => {
+                //don't scroll the page while resizing
+                e.stopPropagation();
+                e.preventDefault();
+
+                if (this.activeElement === gElement.id){
+                    const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+                    this.matrix[gElement.id][0] = this.matrix[gElement.id][0] + 0.01 * delta;
+                    this.matrix[gElement.id][3] = this.matrix[gElement.id][3] + 0.01 * delta;
+                    gElement.setAttribute('transform', 'matrix(' + this.matrix[gElement.id].join(' ') + ')');
+                }
+
+            }, false);
+
+            this.createStar(xOffset, yOffset, length, gElement);
+            this.svg.appendChild(gElement);
+        }
+
+        if(this.textElement) {
+            this.svg.appendChild(this.textElement);
+        }
+
     }
 
     //creates star on random place consisting of [this.params.angles] parts
-    createStar(xOffset, yOffset, length){
+    createStar(xOffset, yOffset, length, parent){
 
 
         const angleOffset =  Math.random() * 360;
         const angles = Math.round(Math.random() * (this.params.maxAngles - this.params.minAngles) + this.params.minAngles);
         const repetitions = Math.random() * (this.params.maxRepetitions - this.params.minRepetitions) + this.params.minRepetitions;
         for (let i=0; i< angles; i++){
-            this.Paths.push('<g>');
-            this.createAngles(i, length, angleOffset, xOffset, yOffset, angles, repetitions);
-            this.Paths.push('</g>');
+            let gElement = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+            parent.appendChild(gElement);
+            this.createAngles(i, length, angleOffset, xOffset, yOffset, angles, repetitions, gElement);
         }
     }
 
-    createAngles(currentAngle, length = this.params.length, angleOffset, xOffset, yOffset, angles, repetitions){
+    createAngles(currentAngle, length = this.params.length, angleOffset, xOffset, yOffset, angles, repetitions, parent){
 
         const angle = new Angle(angles, length, xOffset, yOffset, this.params.color, this.params.backgroundColor);
 
         for (let i=0; i < repetitions; i++){
-            this.Paths.push(angle.createSinglePath(currentAngle + angleOffset, i * 20, this.abstract));
+            let singlePath = angle.createSinglePath(currentAngle + angleOffset, i * 20, this.abstract);
+            parent.appendChild(singlePath);
         }
     }
 
@@ -93,6 +144,8 @@ console.log('abstract', this.params.abstract);
 
         Object.keys(this.params).forEach(key => this.createParamElement(paramBox, key));
 
+        this.createTextInput(paramBox);
+
         const abstractContainer = document.createElement('div');
         abstractContainer.className = 'params';
         paramBox.appendChild(abstractContainer);
@@ -104,8 +157,6 @@ console.log('abstract', this.params.abstract);
         const label = document.createElement('label');
         label.innerText = 'Make it abstract!';
         abstractContainer.appendChild(label);
-
-
 
         element = document.createElement('a');
         element.setAttribute('href', '#');
@@ -125,6 +176,80 @@ console.log('abstract', this.params.abstract);
         element.addEventListener('change', e => this.onAttributeChange(name, e.target.value));
         parentNode.appendChild(element);
         parentNode.appendChild(element);
+    }
+
+    createTextInput(parent){
+        let textInputLabel = document.createElement('label');
+        textInputLabel.setAttribute('class', 'params');
+
+        textInputLabel.innerText = 'Textnachricht: ';
+        let textInput = document.createElement('input');
+
+        textInput.addEventListener('change', (e) => {
+            this.text = e.target.value;
+            if (e.target.value){
+                //remove existing text
+                let ex = document.querySelector('#logo');
+                if(document.querySelector('#logo')){
+                    this.svg.removeChild(ex);
+                }
+
+                let text = document.createElementNS('http://www.w3.org/2000/svg','text');
+                text.id = 'logo';
+                text.style.fontSize = '20px';
+                text.setAttribute('x', '40');
+                text.setAttribute('y', '40');
+                text.setAttribute('fill', this.params.color);
+
+                text.style.userSelect = 'none';
+                text.textContent = e.target.value;
+
+                this.matrix[text.id] = [1, 0, 0, 1, 0, 0];
+
+                //select current element as active
+                text.addEventListener('mousedown', (e) => {
+                    this.activeElement = text.id;
+                    this.matrixX = this.matrix[text.id][4];
+                    this.matrixY = this.matrix[text.id][5];
+                    this.x = e.clientX;
+                    this.y = e.clientY;
+                });
+
+                //deselect
+                text.addEventListener('mouseup', () => {
+                    this.activeElement = null;
+                });
+
+                //move selected element
+                text.addEventListener('mousemove', (e) => {
+                    if(this.activeElement === text.id){
+                        this.matrix[text.id][4] = this.matrixX + e.clientX - this.x;
+                        this.matrix[text.id][5] = this.matrixY + e.clientY - this.y;
+                        text.setAttribute('transform', 'matrix(' + this.matrix[text.id].join(' ') + ')');
+                    }
+                });
+
+                //resize selected element
+                text.addEventListener('mousewheel', (e) => {
+                    //don't scroll the page while resizing
+                    e.stopPropagation();
+                    e.preventDefault();
+
+                    if (this.activeElement === text.id){
+                        const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+                        this.matrix[text.id][0] = this.matrix[text.id][0] + 0.02 * delta;
+                        this.matrix[text.id][3] = this.matrix[text.id][3] + 0.02 * delta;
+                        text.setAttribute('transform', 'matrix(' + this.matrix[text.id].join(' ') + ')');
+                    }
+
+                }, false);
+
+                this.textElement = text;
+            }
+        });
+
+        parent.append(textInputLabel);
+        parent.append(textInput);
     }
 
     onAttributeChange(type, e){
@@ -156,12 +281,14 @@ class Angle{
         const movingAngle = 360 / this.angles * (currentAngle + 0.5);
         const moveX = Math.sin(toRadians(movingAngle)) * (abstract ? Math.random() : 1) * moveLength * this.length / 100;
         const moveY = -Math.cos(toRadians(movingAngle)) * (abstract ? Math.random() : 1) * moveLength * this.length / 100;
-        return '<path transform="translate(' + moveX + ' ' + moveY + ')" id ="angle_' + currentAngle + '" ' +
-            'd="M ' + x0 + ' ' + y0 + ' L ' + this.xOffset + ' ' + this.yOffset + ' L ' + x1 + ' ' + y1 + ' " ' +
-            'd="M ' + this.xOffset + ' ' + this.yOffset + ' L ' + x0 + ' ' + y0 + ' M ' + this.xOffset + ' ' + this.yOffset + ' L ' + x1 + ' ' + y1 + ' " ' +
-            'fill="none" ' +
-            'stroke="' + this.color + '" ' +
-            '/>';
+
+        let pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        pathElement.setAttribute('id', 'angle_' + currentAngle);
+        pathElement.setAttribute('transform', 'translate(' + moveX + ' ' + moveY + ')');
+        pathElement.setAttribute('d', 'M ' + this.xOffset + ' ' + this.yOffset + ' L ' + x0 + ' ' + y0 + ' M ' + this.xOffset + ' ' + this.yOffset + ' L ' + x1 + ' ' + y1 + ' ');
+        pathElement.setAttribute('fill', 'none');
+        pathElement.setAttribute('stroke', this.color);
+        return pathElement;
     }
 }
 
